109a0ac9d038225323205cf07a0b2bc5311f36bd
zulu100 <105020971+zulu100@users.noreply.github.com>
2022-05-05 18:12:32 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+# Se llamar las librerias
+from dash import Dash, html, dcc #Esta es la libreria para el servidor web del data
+import plotly.graph objs as go
+import pandas as pd
+
+# Declarar objetos principales
+app = Dash(__name__)
+
+# Cargar la base de datos
+def serve_layout():
+df = pd.read_excel('datanot.xlsx')
+return html.Div([html.H1(df['Titulo']),
+                 html.Div(df['Noticia1']),
+                 html.Div(df['Noticia2'])])
+                 
+# Declaro metodos principales
+app.layout =serve_layout
+
+if__name__== '__main__':
+#ejecuto el objeto dash en todas las interfaces
+app.run_server(host='0,0,0,0',port=80)
+  
