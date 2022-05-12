9e9b3a09374f37b23671e7e846599cf94ce0c70c
zulu100 <105020971+zulu100@users.noreply.github.com>
2022-05-12 17:10:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Dash(__name__)
 
 # Cargar la base de datos
 def serve_layout():
-df = pd.read_excel('datanot.xlsx')
+df = pd.read_excel('data.xlsx')
 return html.Div([html.H1(df['Titulo']),
                  html.Div(df['Noticia1']),
                  html.Div(df['Noticia2'])])
@@ -18,5 +18,5 @@ app.layout =serve_layout
 
 if__name__== '__main__':
 #ejecuto el objeto dash en todas las interfaces
-app.run_server(host='0,0,0,0',port=80)
+app.run_server(host='0,0,0,0',port=8000)
   
